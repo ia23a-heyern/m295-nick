756a9d6df9f7ea08aec1c0ff9eda41d6b03e792a
ia23a-heyern <146188403+ia23a-heyern@users.noreply.github.com>
2025-06-03 12:07:19 +0200

    Add files via upload

---
+++ m295-nick/src/test/java/org/example/m295nick/M295NickApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.m295nick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M295NickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
